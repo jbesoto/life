00d7d411da5381d4a25485b291468dd42574944e
Juan Becerra <jdb9056@nyu.edu>
2024-02-01 23:53:54 -0500

    Commit program outline

---
+++ life.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// life [rows] [columns] [filename] [generations]
+int main(int argc, char* argv[]) {
+    // Parse user arguments
+    
+    // Set game settings
+
+    // Read the file and create world
+
+    // Print gen 0
+
+    // Play game for `gens` generations
+    return 0;
+}
